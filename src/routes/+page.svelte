658c3808ae294dd355593a8882ec9baf32e46676
olisol <olivia.solis@pm.me>
2022-08-24 21:46:42 +0200

    added small texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,8 @@
 </svelte:head>
 
 <section>
-	<div class="container mx-auto bg-slate-500">
+	<div class="container mx-auto">
+	<div class="absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2"><p>hi@boxybox.me</p></div>
 	<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 		<svg
 			class="hover:animate-spin-fast"
@@ -15,20 +16,18 @@
 			height="173"
 			viewBox="0 0 173 173"
 			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<rect
+			xmlns="http://www.w3.org/2000/svg"><rect
 				x="0.302589"
 				y="86.6185"
 				width="121.902"
 				height="121.902"
 				transform="rotate(-45 0.302589 86.6185)"
-				fill="black"
-			/>
+				fill="black"/>
 		</svg>
 	</div>
     <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <h1 class="text-white">boxy box</h1>
     </div>
+	<div class="absolute bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2"><p>A more human design</p></div>
 </div>
 </section>
